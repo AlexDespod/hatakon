15e775644521f2c63afd0d06b4f6d204d6424b44
AlexDespod <kolesniks559@gmail.com>
2021-01-26 19:40:43 +0200

    eightyh

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     {
         $schedule
             ->command('get:products')
-            ->everyFourMinutes()
+            ->everyTwoHours()
             ->withoutOverlapping();
     }
 
